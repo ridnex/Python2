779b8c87411a30fc78a76386ba6f6af3d21c2356
ridnex <ridnex28@gmail.com>
2022-09-01 20:45:44 +0600

    first preactise

--- pr1/2.py
+++ pr1/2.py
@@ -6,9 +6,10 @@ def index(strb:str) -> int:
         if i=="o" or i=="a" or i=="e" or i =="i" or i == "y" or i=="u":
             return cnt
         cnt+=1
+    return -1
 
 index1 = index(astr)
-if index1 ==0:
+if index1 >=0:
     ans = ""
     for i in range(0, index1):
         ans+=ast[i]
